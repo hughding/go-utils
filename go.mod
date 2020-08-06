2c445e8d20d0b6ff27562602c67c1a2c2e8e2831
hughding <hughding@tencent.com>
2020-08-06 19:35:56 +0800

    opt xorm_sql_util

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-utils
 
 go 1.13
 
-require github.com/stretchr/testify v1.5.1
+require (
+	github.com/emirpasic/gods v1.12.0
+	github.com/stretchr/testify v1.5.1
+)
--- xorm_util/xorm_sql_util.go
+++ xorm_util/xorm_sql_util.go
@@ -1,10 +1,12 @@
-package xorm_util
+package mysql_util
 
 import (
 	"bytes"
 	"fmt"
 	"reflect"
 	"strings"
+
+	"github.com/emirpasic/gods/sets/hashset"
 )
 
 //生成INSERT INTO ON DUPLICATE KEY UPDATE SQL语句和参数
@@ -44,6 +46,54 @@ func GenUpsertSQLAndParams(data interface{}, insertIgnoreFields []string, update
 	return fmt.Sprintf(sqlFormat, getXormTableName(data), insertKeyStr, insertValueStr, updateKeyValueStr), valueList
 }
 
+//生成INSERT INTO ON DUPLICATE KEY UPDATE SQL语句和参数
+func GenBatchUpsertSQLAndParams(datas []interface{}, insertIgnoreFieldSet *hashset.Set, updateIgnoreFieldSet *hashset.Set) (string, []interface{}) {
+	if len(datas) == 0 {
+		return "", nil
+	}
+	//1.定义sqlFormat
+	sqlFormat := "INSERT INTO %s(%s) VALUES %s ON DUPLICATE KEY UPDATE %s"
+
+	//2.获取表名
+	tableName := getXormTableName(datas[0])
+	keyValueMap := getKeyValueMap(datas[0])
+
+	//3.获取插入的和更新的字段
+	var insertFieldList []string
+	var insertValueFmtList []string
+	var updateFieldValueList []string
+	for k := range keyValueMap {
+		if !insertIgnoreFieldSet.Contains(k) {
+			insertFieldList = append(insertFieldList, k)
+			insertValueFmtList = append(insertValueFmtList, "?")
+		}
+		if !updateIgnoreFieldSet.Contains(k) {
+			updateFieldValueList = append(updateFieldValueList, fmt.Sprintf("%s=VALUES(%s)", k, k))
+		}
+	}
+
+	//4.获取单行插入值fmt,形如(?,?,?,?,?,?,?)
+	insertValueFmtStr := fmt.Sprintf("(%s)", strings.Join(insertValueFmtList, ","))
+
+	//5.获取插入值list
+	var insertValueList []interface{}
+	var insertValueFmtStrList []string
+	for _, data := range datas {
+		keyValueMap := getKeyValueMap(data)
+		for _, insertField := range insertFieldList {
+			if value, ok := keyValueMap[insertField]; ok {
+				insertValueList = append(insertValueList, value)
+			}
+		}
+		insertValueFmtStrList = append(insertValueFmtStrList, insertValueFmtStr)
+	}
+
+	insertFieldStr := strings.Join(insertFieldList, ",")
+	updateFieldValueStr := strings.Join(updateFieldValueList, ",")
+	insertValueFmtStrListStr := strings.Join(insertValueFmtStrList, ",")
+	return fmt.Sprintf(sqlFormat, tableName, insertFieldStr, insertValueFmtStrListStr, updateFieldValueStr), insertValueList
+}
+
 func getKeyValueMap(data interface{}) map[string]interface{} {
 	result := make(map[string]interface{})
 	t := reflect.TypeOf(data)
@@ -51,6 +101,9 @@ func getKeyValueMap(data interface{}) map[string]interface{} {
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
 		columnName := getXormColumnName(field.Tag)
+		if columnName == "-" {
+			continue
+		}
 		columnValue := v.Field(i).Interface()
 		if columnName != "" {
 			result[columnName] = columnValue
@@ -65,7 +118,7 @@ func getXormColumnName(tag reflect.StructTag) string {
 	if len(arr) == 0 {
 		return ""
 	}
-	return arr[0]
+	return strings.Trim(arr[len(arr)-1], "'")
 }
 
 func getXormTableName(data interface{}) string {
@@ -79,7 +132,7 @@ func toSnakeCase(str string) string {
 		if ch >= 'A' && ch <= 'Z' {
 			strBuf.WriteString("_")
 			strBuf.WriteRune(ch + 32)
-		}else{
+		} else {
 			strBuf.WriteRune(ch)
 		}
 	}
